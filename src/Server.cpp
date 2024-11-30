bf6981a85e8d15121d6c98bdae52f5cf8c2ec61d
imgdevel <imgdevel-ubuntu@DESKTOP-CFGFLV0.localdomain>
2024-11-30 12:17:32 +0900

    feat: reengineering
    잘못된 설계 처음부터 다시 시작하기
    기존 내용 전부 삭제

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,34 +1,16 @@
 #include "Server.h"
-#include "ThreadPool.h"
-#include <signal.h>
 #include <iostream>
-#include <string>
-#include <stdexcept>
 
-using namespace std;
+Server::Server(int port, int workerCount) : port(port), workerCount(workerCount) {
 
-Server::Server(int port, int workerCount ) : isRunning(true) {
-
-    // todo : reactor와 Thread pool 세팅
-    threadPool = new ThreadPool(workerCount);
-    threadPool->initialize();
-
-    signal(SIGINT, [](int) { cout << "Shutting down server...\n"; });
 }
 
-Server::~Server(){
-    delete threadPool;
+Server::~Server() {
+
 }
 
 void Server::run() {
-    cout << "Server running on port " << port << "...\n";
+    std::cout << "Server is starting on port " << port 
+                << " with " << workerCount << " workers." << std::endl;
 
-    // reactor 실행
-    int n = 0;
-    while (n < 100000)
-    {
-        threadPool->enqueueTask(n);
-        n++;
-    }
-    cout << "종료?? \n";
 }
--- src/Server.h
+++ src/Server.h
@@ -1,20 +1,16 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-class ThreadPool;
-
 class Server {
 public:
     Server(int port, int workerCount);
     ~Server();
+
     void run();
-    
+
 private:
     int port;
     int workerCount;
-    bool isRunning;
-
-    ThreadPool* threadPool;
 };
 
-#endif
+#endif 
