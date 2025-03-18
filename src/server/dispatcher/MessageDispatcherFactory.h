e33365a238414ad8b34359d69cd1b8ea0fdf4acf
ImGdevel <imdlsrks.mc@gmail.com>
2025-03-18 13:44:02 +0900

    refactor: handler/MessageDispatcher 분리 작업-01 (의존성 분리를 위함)
    handler가 MessageDispatcherFactory에 너무 강하게 의존하고 있음.
    추가적으로 MessageDispatcherFactory의 필요성이 의문임
    
    MessageDispatcherFactory를 제거하고 MessageDispatcher에서 handler의 의존성을 줄이기 위함 작업 01

--- src/server/dispatcher/MessageDispatcherFactory.h
+++ src/server/dispatcher/MessageDispatcherFactory.h
@@ -4,16 +4,15 @@
 #include "MessageDispatcher.h"
 #include "HandlerConfigurator.h"
 #include "memory"
+#include "../handler/IEventHandler.h"
 
 class MessageDispatcherFactory {
 public:
-    static std::unique_ptr<MessageDispatcher> createDispatcher(const std::string& format);
+    static std::unique_ptr<MessageDispatcher> createDispatcher(const std::string& format, IEventHandler& handler);
 
 private:
-
-    static std::unique_ptr<MessageDispatcher> createJSONDispatcher();
-    static std::unique_ptr<MessageDispatcher> createJSONRapidDispatcher();
-
+    static std::unique_ptr<MessageDispatcher> createJSONDispatcher(IEventHandler& handler);
+    static std::unique_ptr<MessageDispatcher> createJSONRapidDispatcher(IEventHandler& handler);
 };
 
 #endif
