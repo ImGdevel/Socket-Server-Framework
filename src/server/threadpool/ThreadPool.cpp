c130d25485ca10fa680c49d0d51e910945bafd82
ImGdevel <imdlsrks.mc@gmail.com>
2025-03-20 23:07:40 +0900

    feat: 서버 종료시 제대로 thread가 정리되지 않던 문제 해결 (ref #17)
    - 해당 문제는 worker에서 queue의 작업이 있을때까지 bloking되는 것이 문제 였다.
    - 원래는 nullptr이 들어가면서 종료되어야하지만 두가지 문제점을 고려하지 못해서 삽질함
    
    1) worker queue에 넣은 nullptr을 다른 worker가 먼저 소비하고 종료됨.
    2) 먼저 종료된 worker는 stop이 불리지 않았으므로 대기 상태.
    3) 마치 데드락과 같은 상태가 되어버림...
    
    문제 해결)
    queue안에 종료신호(nullptr)을 thread 개수만큼 이리 삽입후
    worker(thread)에 종료신호를 보냄.
    
    다른 대안이 있는가?)
    worker의 running 변수를 threadpool에서 관리하고 동시에 종료시키면 간단함!
    단점은 worker를 개별적으로 종료시킬 수 없음
    
    해당 방법은 추후 시도해볼 것

--- src/server/threadpool/ThreadPool.cpp
+++ src/server/threadpool/ThreadPool.cpp
@@ -6,15 +6,18 @@
 
 using namespace std;
 
-ThreadPool::ThreadPool(int numThreads) : taskQueue(make_shared<WorkerQueue<std::function<void()>>>()) {
+ThreadPool::ThreadPool(int numThreads) 
+    : running(true), taskQueue(make_shared<WorkerQueue<std::function<void()>>>()) {
     for (int i = 0; i < numThreads; ++i) {
-        workers.emplace_back(make_unique<Worker>(taskQueue));
+        workers.emplace_back(make_unique<Worker>(i, taskQueue));
         workers.back()->start();
     }
 }
 
 ThreadPool::~ThreadPool(){
-    stop();
+    if(running){
+        stop();
+    }
 }
 
 // 작업 Queue에 작업 할당
@@ -27,8 +30,12 @@ void ThreadPool::enqueueTask(const function<void()>& task) {
 
 // ThreadPool내 Worker 정리
 void ThreadPool::stop() {
+    for (size_t i = 0; i < workers.size(); ++i) {
+        taskQueue->push(nullptr);
+    }
     for (auto& worker : workers) {
         worker->stop();
     }
+    running = false;
     Logger::debug("Thread pool shutdown.");
 }
